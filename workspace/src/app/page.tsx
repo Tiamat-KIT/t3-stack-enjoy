c7c785b1d4dbfdcc6f0c94e5fde0ef8836d58c5f
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-12-23 02:32:56 +0900

    node_modules set named volume

--- workspace/src/app/page.tsx
+++ workspace/src/app/page.tsx
@@ -14,6 +14,8 @@ export default async function Home() {
 
   return (
     <HydrateClient>
+    <h3>test</h3>
+    <p>aaa</p>
       {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
