b0d216b7c52bec4a85f15749dd3b830d27b865b1
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-12-22 22:06:12 +0900

    WSL内で生成すればよかったことに気づいた

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Google Calender from Managementer</h1>
+            <nav>
+                <ul className="flex flex-row"> 
+                    <li>
+                        About
+                    </li>
+                    <li>    
+                        Developer
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
